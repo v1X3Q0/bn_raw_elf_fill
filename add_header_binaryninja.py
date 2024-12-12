3b7e7256fe460fcc79a13cb22c8585139c486151
David Riusech <v1X3Q0@gmail.com>
2024-12-12 11:49:07 -0500

    addheader plugin, though binja may already have it

--- add_header_binaryninja.py
+++ add_header_binaryninja.py
@@ -1,4 +1,8 @@
+from binaryninja.interaction import get_open_filename_input, get_text_line_input
+from binaryninja.log import log_alert, log_error, log_info, log_warn
+from binaryninja.binaryview import BinaryView
 import re
+import os
 
 def add_header_by_blob(bv, justCause):
     justCauseNew = ""
@@ -20,8 +24,17 @@ def add_header_by_blob(bv, justCause):
         curName = i.name[0]
         bv.define_user_type(curName, typeList.types[curName])
 
-def add_header(justCause_name):
+def add_header(bv, justCause_name):
     f = open(justCause_name, "r")
     justCause = f.read()
     f.close()
-    add_header_by_blob(justCause)
+    add_header_by_blob(bv, justCause)
+
+def add_header_p(bv: BinaryView):
+    destination_path = get_open_filename_input('target header')
+    
+    if os.path.exists(destination_path) == False:
+        log_error('no header provided')
+        return
+    
+    add_header(bv, destination_path)
